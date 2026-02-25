47ef10b6a53d4f531740cf24899167ac4f6be4cf
AliShafiee2003 <ali.shafiee2002@gmail.com>
2026-02-25 20:46:04 +0330

    Fix GitHub Actions Build: Clone sing-box from SagerNet upstream instead of archived MatsuriDayo fork

--- libs/get_source.sh
+++ libs/get_source.sh
@@ -12,11 +12,36 @@ if [ ! -d "sing-box-extra" ]; then
 fi
 pushd sing-box-extra
 git checkout "$COMMIT_SING_BOX_EXTRA"
+popd
 
+# Clone sing-box from upstream SagerNet (MatsuriDayo fork is archived/broken)
 ENV_SING_BOX_EXTRA=1
 source $SRC_ROOT/libs/get_source_env.sh
-NO_ENV=1 ./libs/get_source.sh
 
+if [ ! -d "sing-box" ]; then
+  git clone --no-checkout https://github.com/SagerNet/sing-box.git
+fi
+pushd sing-box
+git fetch --all
+git checkout "$COMMIT_SING_BOX"
+popd
+
+# Clone sing-quic
+if [ ! -d "sing-quic" ]; then
+  git clone --no-checkout https://github.com/SagerNet/sing-quic.git
+fi
+pushd sing-quic
+git fetch --all
+git checkout "$COMMIT_SING_QUIC"
+popd
+
+# Clone libneko
+if [ ! -d "libneko" ]; then
+  git clone --no-checkout https://github.com/MatsuriDayo/libneko.git
+fi
+pushd libneko
+git fetch --all
+git checkout "$COMMIT_LIBNEKO"
 popd
 
 ####
--- libs/get_source_env.sh
+++ libs/get_source_env.sh
@@ -6,6 +6,6 @@ fi
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
   source libs/get_source_env.sh
   export COMMIT_SING_BOX="f63091d14d8984d53dc9a5563cb72af978b9779e"
-  export COMMIT_SING_QUIC="0bf1f198770e7c0948df48c0f12aab5ff32032df"
+  export COMMIT_SING_QUIC="5f4bd67aff0b6cca68f4eeb511900d4baf3b0b36"
   export COMMIT_LIBNEKO="5277a5bfc889ee7a89462695b0e678c1bd4909b1"
 fi
